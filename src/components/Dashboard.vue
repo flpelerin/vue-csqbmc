6c7e06a614433569d70f2ac65e4806cc996d1666
flpelerin <117655486+flpelerin@users.noreply.github.com>
2024-02-15 09:32:39 +0100

    Initial commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="dashboard">
+  <h1>Dashboard</h1>
+  <ul>
+    <li><router-link to="/link1">Link 1</router-link></li>
+    <li><router-link to="/link2">Link 2</router-link></li>
+    <li><router-link to="/link3">Link 3</router-link></li>
+    <li><router-link to="/link4">Link 4</router-link></li>
+  </ul>
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+name: 'Dashboard',
+};
+</script>
+
+<style scoped>
+/* Add your custom styling here */
+</style>
